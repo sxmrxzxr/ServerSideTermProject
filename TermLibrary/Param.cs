3787089eefe67e75059b59766d3ac7f82211296a
Samuel Joseph Rizer <tue95606@temple.edu>
2017-04-06 15:05:39 -0400

    Created LoginDB.cs, Param.cs, and Login.asmx

---
+++ TermLibrary/Param.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermLibrary
+{
+    class Param
+    {
+        private string paramName;
+        private object paramVal;
+        private SqlDbType paramType;
+
+        public Param(string n, object v, SqlDbType t)
+        {
+            this.paramName = n;
+            this.paramVal = v;
+            this.paramType = t;
+        }
+    }
+}
