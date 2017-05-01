a123f718b30cb1f1178c0ca441c1350374f8c09e
harleybaum <tuf10315@temple.edu>
2017-05-01 18:40:35 -0400

    Add files via upload

---
+++ WebClient/Forum.ascx
@@ -0,0 +1,4 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Forum.ascx.cs" Inherits="WebClient.Forum" %>
+<asp:Label ID="lblForumDescription" runat="server" Font-Underline="True" ForeColor="Black" style="z-index: 1; left: 37px; top: 16px; position: absolute" Text="Label"></asp:Label>
+<asp:TextBox ID="txtQuestionOrAnswer" runat="server" style="z-index: 1; left: 8px; top: 50px; position: absolute"></asp:TextBox>
+
---
+++ WebClient/Forum.ascx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebClient
+{
+    public partial class Forum : System.Web.UI.UserControl
+    {
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        public String ForumDescription
+        {
+            get { return lblForumDescription.Text; }
+            set { lblForumDescription.Text = value; }
+        }
+
+        public string Text
+        {
+            get { return txtQuestionOrAnswer.Text; }
+            set { txtQuestionOrAnswer.Text = value; }
+        }
+
+    }
+}
